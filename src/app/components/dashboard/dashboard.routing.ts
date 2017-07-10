a123a51df7f1461a0aea887ad16a28211d5e57ef
JeongHyeon Ahn <ahnc87@gmail.com>
2017-07-10 15:03:59 +0900

    add routing

---
+++ src/app/components/dashboard/dashboard.routing.ts
@@ -0,0 +1,8 @@
+import { Routes, RouterModule } from '@angular/router';
+import { WardrobeComponent } from 'app/pages/wardrobe/wardrobe.component'
+
+const routes : Routes = [
+  { path : 'wardrobe', component : WardrobeComponent }
+]
+
+export const routing = RouterModule.forRoot(routes);
